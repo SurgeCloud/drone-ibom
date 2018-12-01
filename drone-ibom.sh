36d4d64fc9b1b5acb73ce9ed51d7ed885b4b720a
Tomasz 'Zen' Napierala <tomasz@napierala.org>
2018-12-01 15:53:34 +0100

    Corrected script path, added debug flag

--- drone-ibom.sh
+++ drone-ibom.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --dest-dir $PLUGIN_BASE/$PLUGIN_FOLDER $PLUGIN_PROJECT
+set -x
+/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/generate_interactive_bom.py --dest-dir $PLUGIN_BASE/$PLUGIN_FOLDER $PLUGIN_PROJECT
