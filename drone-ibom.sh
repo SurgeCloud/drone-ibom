fca660a8d8981f5d230de7ffd4e10e731333d51b
Tomasz 'Zen' Napierala <tomasz@napierala.org>
2018-12-01 16:01:38 +0100

    Corrected sript call path

--- drone-ibom.sh
+++ drone-ibom.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/generate_interactive_bom.py --dest-dir $PLUGIN_BASE/$PLUGIN_FOLDER $PLUGIN_PROJECT
+/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --dest-dir $PLUGIN_BASE/$PLUGIN_FOLDER $PLUGIN_PROJECT
