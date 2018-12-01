0a64e7164fa2e6962c2a3bb4a4a25942dcae1743
Tomasz 'Zen' Napierala <tomasz@napierala.org>
2018-12-01 16:40:45 +0100

    Release 0.0.3

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,6 @@ MAINTAINER tomasz@napierala.org
 
 WORKDIR /root
 
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    tree \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-
 COPY drone-ibom.sh /bin/
 COPY InteractiveHtmlBom /usr/src/
 RUN chmod +x /bin/drone-ibom.sh
--- drone-ibom.sh
+++ drone-ibom.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 set -x
-tree /usr/src
-echo
-/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --dest-dir $PLUGIN_BASE/$PLUGIN_FOLDER/$PLUGIN_PROJECT $PLUGIN_PROJECT.kicad_pcb
+/usr/bin/xvfb-run --server-args="-screen 0 1024x768x24" /usr/bin/python2 /usr/src/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --dest-dir $PLUGIN_BASE/$PLUGIN_PROJECT/$PLUGIN_FOLDER $PLUGIN_PROJECT.kicad_pcb
